6a28eea68dc8e2481fd865b10756e1f76f46403b
liubaoquan <liubaoquan0831@163.com>
2015-11-25 17:07:10 +0800

    update spring config properties way

---
+++ web/base/src/main/java/com/github/hualuomoli/base/config/PropertyPlaceholderConfigurer.java
@@ -0,0 +1,10 @@
+package com.github.hualuomoli.base.config;
+
+/**
+ * extend spring PropertyPlaceholderConfigurer
+ * @author hualuomoli
+ *
+ */
+public class PropertyPlaceholderConfigurer extends org.springframework.beans.factory.config.PropertyPlaceholderConfigurer {
+
+}

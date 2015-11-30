0cbe5db6431cee14a024c80a0fe5f05c72b52d2d
hualuomoli <liubaoquan0831@163.com>
2015-11-30 14:37:18 +0800

    add base entity

---
+++ web/modules/base/src/main/java/com/github/hualuomoli/base/entity/BaseEntity.java
@@ -0,0 +1,80 @@
+package com.github.hualuomoli.base.entity;
+
+import java.util.Date;
+
+import com.github.hualuomoli.base.Pagination;
+import com.github.hualuomoli.base.Paginator;
+
+/**
+ * base entity implements Paginator and define common attribute
+ * @author hualuomoli
+ *
+ */
+public abstract class BaseEntity implements Paginator {
+
+	private Pagination Pagination; // pagination
+
+	private String createBy; // data create by which user
+	private Date createDate; // data create date
+	private String updateBy; // data update by which user
+	private Date updateDate; // data update date
+	private Integer status; // data's status
+	private String remark; // data remark
+
+	public Pagination getPagination() {
+		return Pagination;
+	}
+
+	public void setPagination(Pagination pagination) {
+		Pagination = pagination;
+	}
+
+	public String getCreateBy() {
+		return createBy;
+	}
+
+	public void setCreateBy(String createBy) {
+		this.createBy = createBy;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getUpdateBy() {
+		return updateBy;
+	}
+
+	public void setUpdateBy(String updateBy) {
+		this.updateBy = updateBy;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+}
---
+++ web/modules/base/src/main/java/com/github/hualuomoli/base/entity/Status.java
@@ -0,0 +1,12 @@
+package com.github.hualuomoli.base.entity;
+
+/**
+ * Status
+ * @author hualuomoli
+ *
+ */
+public class Status {
+
+	public static final Integer NORMAL = 1; // normal
+
+}
